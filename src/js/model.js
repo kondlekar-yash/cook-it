9880d8fbc3b2701c2dc35c18d417ac184e15a09a
kondlekaryash <yashk9100@gmail.com>
2025-01-30 21:56:18 +1100

    updates and refactoring to MVC

---
+++ src/js/model.js
@@ -0,0 +1,28 @@
+import { async } from "regenerator-runtime";
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}?key=<281097b3-1af9-4ed3-a85d-9f349855f6b0>`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
