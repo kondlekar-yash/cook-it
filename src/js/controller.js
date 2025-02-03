93e6941cbe3adcf08ba27066687146d84c45413d
kondlekaryash <yashk9100@gmail.com>
2025-02-03 14:54:45 +1100

    pub-sub implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -24,6 +24,7 @@ const controlRecipes = async function () {
   }
 };
 
-["hashchange", "load"].forEach((event) =>
-  window.addEventListener(event, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
