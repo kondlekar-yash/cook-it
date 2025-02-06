84334b7be401436e5347489c79359c81705d980b
kondlekaryash <yashk9100@gmail.com>
2025-02-06 21:57:11 +1100

    DOM manipulation algo for better efficiency

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,6 +22,8 @@ const controlRecipes = async function () {
 
     recipeView.renderSpinner();
 
+    resultsView.update(model.getSearchResultsPage()); //* Highlighting the selected recipe
+
     // 1. Loading Recipe
     await model.loadRecipe(id);
 
@@ -57,7 +59,7 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -46,4 +46,32 @@ export default class View {
     this._clear();
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
   }
+
+  update(data) {
+    //* minor algo to update only the changed parts of DOM instead of re-rendering completely
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderErrror();
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll("*"));
+    const curElements = Array.from(this._parentElement.querySelectorAll("*"));
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // Updated TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ""
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+      // Updated ATTRIBUTES
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach((attr) =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
+  }
 }
